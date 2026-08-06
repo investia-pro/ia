e6792c0a7867faba3c9aad394a8eb9f7156a0bae
investia-pro <ratampa797@gmail.com>
2026-08-06 14:54:31 -0300

    Refactor score calculation logic in calcular_score

--- score.py
+++ score.py
@@ -2,28 +2,32 @@ def calcular_score(variacao):
 
     score = 50
 
-    if variacao > 5:
-        score += 40
-    elif variacao > 2:
-        score += 25
-    elif variacao > 0:
+    if variacao >= 5:
+        score += 45
+    elif variacao >= 3:
+        score += 35
+    elif variacao >= 1:
+        score += 20
+    elif variacao >= 0:
         score += 10
-    elif variacao < -5:
-        score -= 40
-    elif variacao < -2:
-        score -= 25
+    elif variacao <= -5:
+        score -= 45
+    elif variacao <= -3:
+        score -= 35
+    elif variacao <= -1:
+        score -= 20
     else:
         score -= 10
 
     score = max(0, min(100, score))
 
-    if score >= 80:
-        recomendacao = "🟢 Compra Forte"
-    elif score >= 60:
-        recomendacao = "🟡 Compra"
-    elif score >= 40:
-        recomendacao = "⚪ Neutro"
+    if score >= 85:
+        recomendacao = '🟢 Compra Forte'
+    elif score >= 70:
+        recomendacao = '🟡 Compra'
+    elif score >= 50:
+        recomendacao = '⚪ Neutro'
     else:
-        recomendacao = "🔴 Venda"
+        recomendacao = '🔴 Venda'
 
     return score, recomendacao
